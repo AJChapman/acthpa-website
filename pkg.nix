4a32d1f85133d06561007d852762a3a4c4b27ad8
Alex Chapman <alex@farfromthere.net>
2019-09-08 16:02:26 +1000

    Update pkg.nix for new name

--- pkg.nix
+++ pkg.nix
@@ -1,14 +1,14 @@
-{ mkDerivation, base, blaze-html, filepath, hakyll, lens, stdenv
-, taggy-lens, text
+{ mkDerivation, base, blaze-html, filepath, hakyll, lens, pandoc
+, stdenv, taggy-lens, text
 }:
 mkDerivation {
-  pname = "acthpa-website2";
+  pname = "acthpa-website";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    base blaze-html filepath hakyll lens taggy-lens text
+    base blaze-html filepath hakyll lens pandoc taggy-lens text
   ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
