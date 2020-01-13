0829355060f11a1b4262ea716bcd19dbddb40cf3
Alex Chapman <alex@farfromthere.net>
2020-01-13 17:58:52 +1100

    Make build more reproducible

--- haskell-package-overrides.nix
+++ haskell-package-overrides.nix
@@ -9,7 +9,8 @@ haskellPackages.override {
     #
     # Use a local copy of another package:
     #   pandoc-wrapper = super.callPackage ../pandoc-wrapper/pkg.nix { };
-    aeson-generic-shorthand = super.callPackage /home/cha748/src/aeson-generic-shorthand/pkg.nix { };
-    slick = super.callPackage /home/cha748/ext/slick/pkg.nix { };
+
+    # Get this from https://github.com/AJChapman/aeson-generic-shorthand
+    aeson-generic-shorthand = super.callPackage ../aeson-generic-shorthand/pkg.nix { };
   };
 }
