2c05a473624551a194d48d3693f872c0c2f5330a
Alex Chapman <alex@farfromthere.net>
2020-01-16 12:13:26 +1100

    Make the deploy script more flexible in how it can be used

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,30 +17,47 @@ ACTHPA_DIR=$(readlink -f "${BIN_DIR}/..")
 MOUNT=${MOUNT:-"${ACTHPA_DIR}/../acthpa.org"}
 DEPLOY_TO=${DEPLOY_TO:-"${MOUNT}/public_html/"}
 
-case "$(basename "$0")" in
-    deploy.sh)
-        # Make sure we're in the acthpa-website base dir
-        cd "${ACTHPA_DIR}"
+# Make sure we're in the acthpa-website base dir
+cd "${ACTHPA_DIR}"
 
+case "$(basename "$0")" in
+    update.sh)
         # Make sure the repo is up to date
         git pull
-
-        # Start this script running as nix-deploy.sh in a nix-shell
-        nix-shell --run "${BIN_DIR}/nix-deploy.sh"
         ;;
 
-    nix-deploy.sh)
-        # Now that we're running inside nix-shell, build the site and deploy it
-        cd "${ACTHPA_DIR}"
-        cabal new-build scrape
-        cabal new-exec scrape
-        cabal new-build build-site
+    scrape.sh)
+        # Start this script running as nix-build.sh in a nix-shell
+        nix-shell --run "${BIN_DIR}/nix-build.sh scrape"
+        nix-shell --run "${BIN_DIR}/nix-exec.sh scrape"
+        ;;
 
-        # Remove any previous build
+    build-deploy.sh)
+        ${BIN_DIR}/build.sh
+        ${BIN_DIR}/deploy.sh
+        ;;
+
+    scrape-build-deploy.sh)
+        ${BIN_DIR}/scrape.sh
+        ${BIN_DIR}/build-deploy.sh
+        ;;
+
+    update-scrape-build-deploy.sh)
+        ${BIN_DIR}/update.sh
+        ${BIN_DIR}/scrape-build-deploy.sh
+        ;;
+
+    build.sh)
+        # Start this script running as nix-build.sh in a nix-shell
+        nix-shell --run "${BIN_DIR}/nix-build.sh build-site"
+
+        # Remove any previous build so we don't get caching / old data problems
         rm -rf gen/ .shake/
 
-        cabal new-exec build-site
+        nix-shell --run "${BIN_DIR}/nix-exec.sh build-site"
+        ;;
 
+    deploy.sh)
         # Make sure the WebDAV directory is mounted
         mount "${MOUNT}" || true
 
@@ -48,6 +65,17 @@ case "$(basename "$0")" in
 
         # Unmount again
         umount "${MOUNT}" || true
+        ;;
+
+    nix-exec.sh)
+        TARGET="$1"
+        cabal new-exec "$TARGET"
+        ;;
+        
+    nix-build.sh)
+        TARGET="$1"
+        # Now that we're running inside nix-shell we can call cabal
+        cabal new-build "$TARGET"
         ;;
 
     *)
