a195c460db8bac562a3e217ed3619868c6bc15eb
Alex Chapman <alex@farfromthere.net>
2020-01-26 16:53:24 +1100

    Relayout images once loaded (sites page).

--- site/js/main.js
+++ site/js/main.js
@@ -188,12 +188,16 @@
 	};
 
         var gridMasonry = function() {
-            $('.grid').masonry({
+            var $grid = $('.grid').masonry({
               // set itemSelector so .grid-sizer is not used in layout
               itemSelector: '.grid-item',
               // use element for option
               columnWidth: '.grid-sizer',
               percentPosition: true
+            });
+            // layout Masonry after each image loads
+            $grid.imagesLoaded().progress(function() {
+              $grid.masonry('layout');
             })
         };
 	
